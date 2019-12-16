7ef5126f64b538b1a4a00a606192cb3ccadee9f7
codeInn001 <emmaalege@gmail.c0m>
2019-12-17 00:07:54 +0100

    added more msg

--- countdown.js
+++ countdown.js
@@ -12,15 +12,31 @@ let xmasMessage = [
   'Let the spirit of love gently fill our hearts and homes. in the loveliest of celebrations may you find many reasons for hapiness.',
   'A silent night, a star above, a blessed gift of hope and love. A merry Christmas to you and your family.',
   'May your Christmas sparkle with moments of love, laughter and goodwill and may the year ahead be full of contentment and joy.',
-  'Wishing you a joyous Christmas and a happy and prosperous New Year.'
+  'Wishing you a joyous Christmas and a happy and prosperous New Year.',
+  'Wish you all the best this holiday season and throughout the year,Merry Christmas!',
+  'I wish this holiday season you would receive Love, peace and joy for the whole year,Merry Christmas!',
+  'If I could tell Santa what to give you,It would be happiness and peace Not just this Christmas,but for the entire year.',
+  'May this holiday season be full of surprises and cheers,simply because you deserved the best',
+  'May your life be filled with the blessings of Christmas And may it always be yours.',
+  'May this year’s Christmas be the best Christmas ever.Warmest wishes to you and your family.Have a great and wonderful Christmas and New Year!',
+  'Celebrate this day with a heart Filled with happiness, cheer and love Share the blessings to others for that is the true meaning of Christmas Have a happy holiday!',
+  'Listen to the whispers of the wind this Christmas. They carry a message of love, peace, hope and happiness for you. Wishing you a merry Christmas.',
+  'I pray this season brings you unending bliss, Peace that transcends your soul and laughter for all your days. Merry Christmas.',
+  'I pray this season brings to you the courage to brave new storms, Joy that numbs all pain and a love that envelops you forever. Best wishes and a very Merry Christmas to you',
+  'This Christmas, I wish you all the best gifts in life – The gift of happiness The gift of love The gift of peaceMerry Christmas and Happy New Year!',
+  'May you be blessed with gifts of hope, love and peace. Merry Christmas to you and your family.',
+  'May this season of love be the beginning of a blessed and peaceful year ahead. Merry Christmas and Happy New Year.',
+  'I wish you the unconditional love of God in all the dreams of your heart and your everyday prayers. Merry Christmas.',
+  'I wish you nothing but the best of the joyous Christmas season. Embrace every blessing that will come your way. Merry Christmas!',
+  'Amidst the cold weather and, misty atmosphere I send nothing but the warmest Christmas greetings this Yuletide season.Have a very merry Christmas!',
 ]
-let random = Math.floor(Math.random() * xmasMessage.length);
-let submitBtn = document.querySelector('button');
-let nameInput = document.querySelector('.custom-name');
-let customAlert = document.querySelector('.custom-alert');
-let media = document.querySelector('audio');
-let invalidInput = document.querySelector('.invalid-input');
-let playBtn = document.querySelector('.play')
+const random = Math.floor(Math.random() * xmasMessage.length);
+const submitBtn = document.querySelector('button');
+const nameInput = document.querySelector('.custom-name');
+const customAlert = document.querySelector('.custom-alert');
+const media = document.querySelector('audio');
+const invalidInput = document.querySelector('.invalid-input');
+const playBtn = document.querySelector('.play')
 playBtn.style.display = 'none';
 
 function updateTime(){
@@ -85,5 +101,5 @@ window.addEventListener('mousedown',playMedia);
 window.addEventListener('keyup',function(e){
   if(e.keyCode == '13' ){
     displayXmasMsg();
-  }else{console.log(e)}
+  }else{return;}
 })
